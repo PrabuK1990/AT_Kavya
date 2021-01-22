bcf898e0604d1adb3af2674c08124c22e9e9e61f
Prabu Kumaravel <prabu.kumaravel@ext.deposit-solutions.com>
2021-01-22 12:50:28 +0530

    locators added

--- Test1/src/test/java/controls/marriottControls.java
+++ Test1/src/test/java/controls/marriottControls.java
@@ -6,5 +6,16 @@ public interface marriottControls {
 	public String Email_Textbox = "user-id"; //Id
 	public String Password_Textbox = "password"; //Name
 	public String SignIn = "//button[text()=' Sign In ']"; //xpath
+	public String Destination = "destinationAddress.destination"; //name
+	public String StartDate = "//input[@placeholder='Check-in']"; //xpath
+	public String EndDate = "//input[@placeholder='Check-out']"; //xpath
+	public String RoomsAndGuests = "//p[@class='l-display-text']"; //xpath
+	public String SpecialRates = "//span[text()='None']"; //xpath
+	public String UsePoints = "useRewardsPoints"; //name
+	public String FindHotels = "///div[@id='rewards-use-search']/ancestor::div[2]/following-sibling::div/button"; //xpath
+	
+	//add 14 locators for sinin page
+	//add locators for create account page
+	//add 21 locators for mytrips
 
 }
